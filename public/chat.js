a42e23de51c76901ded26171c3e7a425b9640d4d
Josiah Burnette <106935232+Jo-Burn@users.noreply.github.com>
2025-07-09 18:05:05 -0400

    Summery

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,19 @@
 console.log('Hello app')
+let send = document.getElementById('send')
+let input = document.getElementById('input')
+let enter = document.getElementById("chat-box");
+
+enter.addEventListener('keypress', 
+    function(event) {
+        if(event.key === "Enter") {
+           // console.log('work')
+            event.preventDefault();
+            enter.click(sendMessage())
+        }
+    }
+)
+//send.addEventListener("click", sendMessage())
+
+function sendMessage() {
+    console.log('message sent')
+}
